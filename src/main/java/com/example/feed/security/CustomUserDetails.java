bfd7635b4249a81e7478c7a9dcf5e93574f0ceca
Mybread2 <ckwnsgh4@naver.com>
2025-05-27 15:39:24 +0900

    Spring security + jwt 기반 인증 시스템 구현 / 로그인 기능 구현

--- src/main/java/com/example/feed/security/CustomUserDetails.java
+++ src/main/java/com/example/feed/security/CustomUserDetails.java
@@ -3,7 +3,6 @@ package com.example.feed.security;
 import com.example.feed.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Collections;
