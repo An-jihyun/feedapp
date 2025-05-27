ffb75329181c26d7978f6e06b5a5dfec381c0483
teopteop <teopteop0718@gmail.com>
2025-05-28 01:35:57 +0900

    refactor: 오용된 어노테이션 변경 @Column -> @NotNull

--- src/main/java/com/example/feed/dto/post/request/CreatePostRequestDto.java
+++ src/main/java/com/example/feed/dto/post/request/CreatePostRequestDto.java
@@ -1,17 +1,17 @@
 package com.example.feed.dto.post.request;
 
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreatePostRequestDto {
 
-    @Column(nullable = false)
+    @NotNull(message = "제목을 입력하세요")
     @Size(max = 20, message = "제목은 20자 이내로 작성가능합니다.")
     private String title;
 
-    @Column(nullable = false)
+    @NotNull(message = "내용을 입력하세요")
     @Size(min = 5, max = 200, message = "작성글은 5~200자 이내로 작성가능합니다.")
     private String content;
 
