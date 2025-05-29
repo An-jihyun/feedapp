db8b4d158ce6b05e19ac503f882a519ee243bbd0
Mybread2 <ckwnsgh4@naver.com>
2025-05-29 11:26:44 +0900

    feat: AuthService - 로그인 기능 예외처리 기능 구현

--- src/main/java/com/example/feed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/feed/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.example.feed.common.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -81,5 +83,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex, HttpServletRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), request.getServletPath());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse("이메일 또는 비밀번호가 일치하지 않습니다.", request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameNotFound(HttpServletRequest request) {ErrorResponse errorResponse = new ErrorResponse("이메일 또는 비밀번호가 일치하지 않습니다.", request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
