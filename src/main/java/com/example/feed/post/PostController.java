b5c4a3297605d98c244e6921712a3af700d6f3c7
teopteop <teopteop0718@gmail.com>
2025-05-30 00:33:37 +0900

    refactor: 204 사용시 응답에 포함된 데이터를 전부 무시함 -> 200 상태코드로 변경

--- src/main/java/com/example/feed/post/PostController.java
+++ src/main/java/com/example/feed/post/PostController.java
@@ -48,7 +48,7 @@ public class PostController {
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
         postService.delete(id, userDetails);
-        return new ResponseEntity<>(new ApiResponse<>("정상적으로 게시물이 삭제되었습니다.", null), HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(new ApiResponse<>("정상적으로 게시물이 삭제되었습니다.", null), HttpStatus.OK);
     }
 
     //게시물 전체 조회 페이지네이션, 검색조건(시작, 종료일) 포함
