aff237af4025063dcc42528e30020d45deda3dac
zeroColaa <sinyoung556@gmail.com>
2025-05-31 11:34:29 +0900

    feat: 댓글 삭제 시 관련 좋아요 데이터 선삭제 로직 추가

--- src/main/java/com/example/feed/comment/CommentService.java
+++ src/main/java/com/example/feed/comment/CommentService.java
@@ -3,6 +3,8 @@ package com.example.feed.comment;
 import com.example.feed.comment.dto.CommentResponseDto;
 import com.example.feed.comment.dto.CreateCommentRequestDto;
 import com.example.feed.comment.dto.UpdateCommentRequestDto;
+import com.example.feed.like.LikeService;
+import com.example.feed.like.LikeTargetType;
 import com.example.feed.post.Post;
 import com.example.feed.user.User;
 import com.example.feed.exception.CommentNotFoundException;
@@ -26,6 +28,7 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentDomainUtils commentDomainUtils;
+    private final LikeService likeService;
 
 
     public CommentResponseDto createComment(Long postId, CreateCommentRequestDto requestDto, String email) {
@@ -54,15 +57,22 @@ public class CommentService {
 
 
     public void deleteComment(Long commentId, Long userId) {
+        likeService.deleteAllLikesByTargetId(LikeTargetType.COMMENT, commentId);
         commentDomainUtils.validateAndGetComment(commentId, userId).softDelete();
     }
 
     //상위 도메인 호출 용(soft delete)
     public void softDeleteCommentsByPostId(Long postId) {
-        commentRepository.findByPostIdAndDeletedFalse(postId).forEach(Comment::softDelete);
+        commentRepository.findByPostIdAndDeletedFalse(postId).forEach(comment -> {
+            likeService.deleteAllLikesByTargetId(LikeTargetType.COMMENT, comment.getId()); // 좋아요 먼저 삭제
+            comment.softDelete(); // 댓글 삭제
+        });
     }
     public void softDeleteCommentsByUserId(Long userId) {
-        commentRepository.findByUserIdAndDeletedFalse(userId).forEach(Comment::softDelete);
+        commentRepository.findByUserIdAndDeletedFalse(userId).forEach(comment -> {
+            likeService.deleteAllLikesByTargetId(LikeTargetType.COMMENT, comment.getId()); // 좋아요 먼저 삭제
+            comment.softDelete(); // 댓글 삭제
+        });
     }
 
 
